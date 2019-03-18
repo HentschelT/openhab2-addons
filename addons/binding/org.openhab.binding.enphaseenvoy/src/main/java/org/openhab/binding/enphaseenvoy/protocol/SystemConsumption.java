9e4d4930184cad0652927194995d3a9927c780d0
Sameer Morar <smorar@gmail.com>
2019-03-19 01:08:36 +1000

    Add support for reading consumption data from enphase envoy

---
+++ addons/binding/org.openhab.binding.enphaseenvoy/src/main/java/org/openhab/binding/enphaseenvoy/protocol/SystemConsumption.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+package org.openhab.binding.enphaseenvoy.protocol;
+
+/**
+ * @author smorar
+ *
+ */
+public class SystemConsumption {
+
+    public long wattHoursToday;
+    public long wattHoursSevenDays;
+    public long wattHoursLifetime;
+    public long wattsNow;
+}
